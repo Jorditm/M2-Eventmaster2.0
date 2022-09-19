ea4091f10c5d32e613e5a4d0cb4b949fb125aef6
Jordi TM <jorditorrasm@gmail.com>
2022-09-19 13:57:44 +0200

    index.js app connect changed

--- index.js
+++ index.js
@@ -1,69 +1,71 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const bodyParser = require('body-parser');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
 
-const session = require('express-session'); 
-const MongoStore = require('connect-mongo')(session); 
-var back = require('express-back');
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+var back = require("express-back");
 
-const indexRouter = require('./routes/index');
-const authRouter = require('./routes/auth');
-const eventRouter = require ('./routes/event');
-const favoritoRouter= require('./routes/favorito');
-
+const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
+const eventRouter = require("./routes/event");
+const favoritoRouter = require("./routes/favorito");
 
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(x => {
+  .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
     );
   })
-  .catch(err => {
-    console.error('Error connecting to mongo', err);
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
   });
-
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 // Middleware Setup
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { maxAge: 6000000 },
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 24 * 60 * 60 // 1 day
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true, maxAge: 6000000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60, // 1 day
+    }),
   })
-}));
+);
 
-app.use(session({
-  secret: 'super secret'
-}));
+app.use(
+  session({
+    secret: "super secret",
+  })
+);
 app.use(back());
 
 app.use((req, res, next) => {
@@ -77,32 +79,27 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
-
-app.use('/', indexRouter);
-app.use('/auth', authRouter);
-app.use('/event', eventRouter);
-app.use('/favoritos', favoritoRouter);
-
-
-
-
+app.use("/", indexRouter);
+app.use("/auth", authRouter);
+app.use("/event", eventRouter);
+app.use("/favoritos", favoritoRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
